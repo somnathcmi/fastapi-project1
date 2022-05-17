b5fd719b12a570dab6cc443c11f9476806c91fc9
somnathcmi <somnathcmi@gmail.com>
2022-05-17 14:52:42 +0530

    last commit

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
+async def root():
+    return "This is root of FastAPI webapi deployed on deta by somnath"
+
+@app.get("/info")
 async def root():
     return {"message": "Hello World"}
